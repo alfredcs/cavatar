27c82e33b43cc0048fb55aea9adab069991fb1e5
alfredcs <alfredcs@yahoo.com>
2024-08-05 21:19:47 +0000

    Update

--- demo/mmrag/home.py
+++ demo/mmrag/home.py
@@ -385,19 +385,32 @@ elif (record_audio_bytes and len(voice_prompt) > 1):
             prompt=voice_prompt if prompt==' ' else prompt
             st.session_state.messages.append({"role": "user", "content": prompt})
             st.chat_message("user").write(prompt)  
-            if 'med42' in option.lower():
+
+            action = classify_query(prompt, 'image generation, image upscaling, news, others', 'anthropic.claude-3-haiku-20240307-v1:0')
+            if 'generation' in action:
+                #option = 'amazon.titan-image-generator-v1' #'stability.stable-diffusion-xl-v1:0' # Or 'amazon.titan-image-generator-v1'
+                #base64_str = bedrock_imageGen(option, prompt, iheight=1024, iwidth=1024, src_image=None, image_quality='premium', image_n=1, cfg=7.5, seed=452345)
+                #new_image = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
+                option = 'SD3 Medium' 
+                new_image = gen_photo_bytes(prompt)
+                st.image(new_image, output_format="png", use_column_width='auto')
+                msg = ' '
+            elif 'upscaling' in action and hasattr(locals(), 'new_image'):
+                try:
+                    new_image2 = upscale_image_bytes(new_image.getvalue())
+                    st.image(new_image, output_format="png", use_column_width='auto')
+                    st.image(new_image2, output_format="png", use_column_width='auto')
+                    msg = "\n\n ✒︎***Content created by using:*** Aura V2 " + f", Latency: {(time.time() - start_time) * 1000:.2f} ms" 
+                except:
+                    msg = "Server timeout. Please check imahe format and size and retry. " + f", Latency: {(time.time() - start_time) * 1000:.2f} ms" 
+                    pass
+            elif 'med42' in option.lower():
                 msg=tgi_textGen2('http://infs.cavatar.info:7861/', prompt, max_token, temperature, top_p, top_k)
             elif 'gpt-4' in option:
                 msg = openai_textGen(option, prompt, max_token, temperature, top_p)
             elif 'claude-3-5' in option:
                 msg = anthropic_textGen(option, prompt, max_token, temperature, top_p, top_k, stop_sequences)
             #elif 'generate imagetextGen(option, prompt, max_token, temperature, top_p, top_k, stop_sequences)' in classify_query(prompt, 'generate image, news, others', 'anthropic.claude-3-haiku-20240307-v1:0'):
-            elif 'generate' in classify_query(prompt, 'generate image, news, others', 'anthropic.claude-3-haiku-20240307-v1:0'):
-                option = 'amazon.titan-image-generator-v1' #'stability.stable-diffusion-xl-v1:0' # Or 'amazon.titan-image-generator-v1'
-                base64_str = bedrock_imageGen(option, prompt, iheight=1024, iwidth=1024, src_image=None, image_quality='premium', image_n=1, cfg=7.5, seed=452345)
-                new_image = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
-                st.image(new_image,width=512)#use_column_width='auto')
-                msg = ' '
             else:
                 msg=bedrock_textGen(option, prompt, max_token, temperature, top_p, top_k, stop_sequences)
             msg += "\n\n ✒︎***Content created by using:*** " + option + f", Latency: {(time.time() - start_time) * 1000:.2f} ms" + f", Tokens In: {estimate_tokens(prompt, method='max')}, Out: {estimate_tokens(msg, method='max')}"
@@ -408,15 +421,25 @@ else:
     if prompt := st.chat_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
-        
-        if 'generate' in classify_query(prompt, 'generate image, news, others', 'anthropic.claude-3-haiku-20240307-v1:0'):
+
+        action = classify_query(prompt, 'image generation, image upscaling, news, others', 'anthropic.claude-3-haiku-20240307-v1:0')
+        if 'generation' in action:
             #option = 'amazon.titan-image-generator-v1' #'stability.stable-diffusion-xl-v1:0' # Or 'amazon.titan-image-generator-v1'
             #base64_str = bedrock_imageGen(option, prompt, iheight=1024, iwidth=1024, src_image=None, image_quality='premium', image_n=1, cfg=7.5, seed=452345)
             #new_image = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
-            option = 'DreamLike V2.0' 
+            option = 'SD3 Medium' 
             new_image = gen_photo_bytes(prompt)
             st.image(new_image, output_format="png", use_column_width='auto')
             msg = ' '
+        elif 'upscaling' in action and hasattr(locals(), 'new_image'):
+            try:
+                new_image2 = upscale_image_bytes(new_image.getvalue())
+                st.image(new_image, output_format="png", use_column_width='auto')
+                st.image(new_image2, output_format="png", use_column_width='auto')
+                msg = "\n\n ✒︎***Content created by using:*** Aura V2 " + f", Latency: {(time.time() - start_time) * 1000:.2f} ms" 
+            except:
+                msg = "Server timeout. Please check imahe format and size and retry. " + f", Latency: {(time.time() - start_time) * 1000:.2f} ms" 
+                pass
         elif 'llama3-med42-8b' in option.lower():
             msg = tgi_textGen2('http://infs.cavatar.info:7861/', prompt, max_token, temperature, top_p, top_k)
         elif 'gpt-4' in option:
